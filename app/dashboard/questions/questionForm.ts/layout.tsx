b89cbf8dbce2422a5ba7efd735625c5294f041aa
Afeefuddin <afeefud2004din@gmail.com>
2024-06-30 04:32:52 +0530

    minor

--- app/dashboard/questions/questionForm.ts/layout.tsx
+++ app/dashboard/questions/questionForm.ts/layout.tsx
@@ -2,7 +2,7 @@
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
-import "../../../../global.css";
+import "../../../global.css";
 
 const fontHeading = Inter({
   subsets: ["latin"],
