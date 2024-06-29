948b19e88009d269606dc95b870acb88b99deebb
subharthi75 <subharthi75@gmail.com>
2024-06-30 00:22:02 +0530

    plan change stripe

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -22,9 +22,9 @@ export async function POST(
           price_data: {
             currency: "usd",
             product_data: {
-              name: "3moplan",
+              name: "MockMate Pro",
             },
-            unit_amount: 100,
+            unit_amount: 500,
           },
           quantity: 1,
         }
